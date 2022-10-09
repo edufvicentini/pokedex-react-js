c1b50d91f7c437b5b11b9054f83d009e5acf3252
Eduardo Frota Vicentini <eduardofvicentini@gmail.com>
2022-10-09 15:58:28 -0300

    under construction commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,25 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
+import charmander from './assets/charmander.png'
+import { GitHub }  from 'react-feather'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <div className="App">
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
+        <a href="https://pokeapi.co/" target="_blank">
+          <img src={charmander} className="pokemon" alt="Cute Charmander" />
         </a>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
+      
+      <h1>Project under construction</h1>
+      
+      <div className="footer"> 
+        <a className="github-link" href='https://github.com/edufvicentini/pokedex-react-js' target="_blank">
+          <GitHub className="github-icon"/>  
+          <span>Click here to learn more about the project!</span>
+        </a>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </div>
   )
 }
